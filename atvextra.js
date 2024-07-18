9deca7c60521826a3e3543a4f4520361c72e55a7
Renanlucass <renanlucas0014@gmail.com>
2024-07-18 10:45:59 -0300

    a

---
+++ atvextra.js
@@ -0,0 +1,10 @@
+// Lista
+
+function add() {
+    const input = document.querySelector("#input");
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = "";
+  }
